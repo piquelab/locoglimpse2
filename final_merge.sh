06f646af0d3a685022c16ab404472007cb9f526d
rpique <rpique@gmail.com>
2024-01-06 08:46:24 -0500

    add comments

--- final_merge.sh
+++ final_merge.sh
@@ -1,4 +1,5 @@
-bcftools merge GLIMPSE_ligate/HO-*.bcf --threads 12 | bcftools view -v snps -m2 -M2 --threads 4 -Oz -o ./merge.ref.vcf.gz
+## remember to change the prefix. 
+bcftools merge GLIMPSE_ligate/AL-*.bcf --threads 12 | bcftools view -v snps -m2 -M2 --threads 4 -Oz -o ./merge.ref.vcf.gz
 bcftools index merge.ref.vcf.gz
 bcftools merge GLIMPSE_ligate/HO-*.bcf --threads 4 | bcftools view -v snps -m2 -M2 -i 'INFO/MAF>0' --threads 4 -Oz -o ref.ac1.vcf.gz  
 bcftools index ref.ac1.vcf.gz 
