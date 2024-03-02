0102ad429650001383b474e7005daef673af74b4
rpique <rpique@gmail.com>
2024-03-01 22:22:21 -0500

    updated bam to fastq for LP samples

--- makefastq.sh
+++ makefastq.sh
@@ -4,16 +4,21 @@ mkdir -p fastq
 ##cp /wsu/home/groups/piquelab/OurData/genewiz/30-887274028/00_fastq/*.fastq.gz ./fastq/
 ##cp /tier2/home/groups/pique20/old.piquelab/OurData/genewiz/*/00_fastq/AL* ./fastq/
 
+##cd fastq; 
+##find /wsu/home/groups/piquelab/OurData/genewiz/ -name 'LP*R[1-2]*.fastq.gz' -print0 | parallel -0 'ln -s {}'
+##rename _S1_L001_R _R *.fastq.gz
+
 ##folder=/nfs/rprdata/ALOFT/gencove.AloftHoldPrb.2019-03-22/bam
-folder=/rs/rs_grp_scaloft/locoglimpse2/bamfix
+##folder=/rs/rs_grp_scaloft/locoglimpse2/bamfix
 ##folder=/nfs/rprdata/ALOFT/gencove/bam/
+folder=/nfs/rprdata/ALOFT/gencove.old/bam/
 outfolder=./fastq
 
 
 
 module load samtools
 
-for file in $folder/AL*.bam; do
+for file in $folder/LP*.bam; do
     bam=${file##$folder/}
     sample=${bam%%.bam}
     if [ ! -f "$outfolder/slurm.${sample}.out" ]; then 
